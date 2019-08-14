396853777df673cf1bb310ded53422d83ca9771a
Luke Carl Thompson <lukecarlthompson@gmail.com>
2019-08-14 22:33:20 +0800

    more demos

--- _src/js/index.js
+++ _src/js/index.js
@@ -1,19 +1,54 @@
 import slippySlider from "../../dist/slippyslider.esm.js";
 
-console.log("Index.js file 😎");
+// Center mode demo
 
-const slider = new slippySlider({
-  slider: ".slippy-slider",
+const sliderCenter = new slippySlider({
+  slider: ".slippy-slider--center",
   slides: ".slippy-slider__slide",
   center: true,
 });
 
-const prevButton = slider.slider.querySelector(".control-prev");
-const nextButton = slider.slider.querySelector(".control-next");
+const prevButton = document.querySelector(".demo--center .control-prev");
+const nextButton = document.querySelector(".demo--center .control-next");
 
 prevButton.addEventListener("click", () => {
-  slider.moveTo("prev");
+  sliderCenter.moveTo("prev");
 });
 nextButton.addEventListener("click", () => {
-  slider.moveTo("next");
+  sliderCenter.moveTo("next");
+});
+
+// Left aligned mode demo
+const sliderLeft = new slippySlider({
+  slider: ".demo--left .slippy-slider",
+  slides: ".slippy-slider__slide",
+  center: false,
+});
+
+const sliderLeftPrevButton = document.querySelector(".demo--left .control-prev");
+const sliderLeftNextButton = document.querySelector(".demo--left .control-next");
+
+sliderLeftPrevButton.addEventListener("click", () => {
+  sliderLeft.moveTo("prev");
+});
+sliderLeftNextButton.addEventListener("click", () => {
+  sliderLeft.moveTo("next");
+});
+
+
+// Responsive demo
+const sliderResponsive = new slippySlider({
+  slider: ".demo--responsive .slippy-slider",
+  slides: ".slippy-slider__slide",
+  center: true,
+});
+
+const responsivePrevButton = document.querySelector(".demo--responsive .control-prev");
+const responsiveNextButton = document.querySelector(".demo--responsive .control-next");
+
+responsivePrevButton.addEventListener("click", () => {
+  sliderResponsive.moveTo("prev");
+});
+responsiveNextButton.addEventListener("click", () => {
+  sliderResponsive.moveTo("next");
 });
